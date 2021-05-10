61c232581fdf25a599e211c97fd7f17a89320492
Aditya Kanfade <adityaakanfade@gmail.com>
2021-05-11 01:13:21 +0530

    Add ui using HTML and CSS and refactor javascript

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Initializing user score and computer score
+let userScore = 0;
+let computerScore = 0;
+
+// selecting each button
+const buttons = document.querySelectorAll('button');
+
+const comments = document.querySelector('.comments');
+
+const userScoreText = document.querySelector('.user-score');
+
+const computerScoreText = document.querySelector('.computer-score');
+
 // function for playing on computer's part
 function computerPlay() {
     // Selecting a random number from 0 - 2 for selecting a weapon
@@ -17,94 +30,75 @@ function computerPlay() {
     }
 }
 
-// function to capitalize first letter with a string parameter
-function firstLetterCapital(text) {
-    // get a character from first letter of the string
-    let firstLetter = text.charAt(0);
-   
-    // slice the rest of the characters
-    let remainingLetters = text.slice(1);
-    
-    // convert the character to upper case
-    firstLetter = firstLetter.toUpperCase();
-   
-    // join both the strings 
-    text = firstLetter + remainingLetters;
-
-    // return the updated string
-    return text;
+// Updating the scores
+function updateScores(result) {
+    if (result === 'win') {
+        userScore++;
+        userScoreText.textContent = 'Player: ' + userScore;
+    } else if (result === 'loss') {
+        computerScore++;
+        computerScoreText.textContent = 'Computer: ' + computerScore;
+    }
 }
 
-// function to play a single round of the game with two parameters playerSelection and computerSelection
-function playRound(playerSelection, computerSelection) {
-    // convert all characters of playerSelection to lower case
-    playerSelection = playerSelection.toLowerCase();
+//change comment
+function changeComment(result, playerSelection, computerSelection) {
+    if (result === 'win') {
+        comments.textContent = 'You won this round! ' + playerSelection + ' beats ' + computerSelection;
+    } else if (result === 'loss') {
+        comments.textContent = 'You lost this round! ' + computerSelection + ' beats ' + playerSelection;
+    } else {
+        comments.textContent = 'This round was a draw';
+    }
+}
 
-    // declare a variable to store the result. Initially it is an empty string
-    let result = '';
+// function to play a round 
+function playRound(e) {
+    // getting player selection from the class name of event
+    const playerSelection = e.target.className;
 
+    // getting computer selection
+    const computerSelection = computerPlay();
+    
     // check the values of the two parameters for the result
-    // if playerSelection == computerSelection result is draw
     /* else if (playerSelection == 'rock' AND computerSelection == 'paper') OR (playerSelection == 'paper' AND computerSelection == 'scissor') OR (playerSelection == 'scissor' AND computerSelection == 'rock')
-    result is loss + playerSelection + 'loses to ' + computerSelection
+    change comment and update scores for loss
     */
     /* else if (playerSelection == 'paper' AND computerSelection == 'rock') OR (playerSelection == 'scissor' AND computerSelection == 'paper') OR (playerSelection == 'rock' AND computerSelection == 'scissor')
-    result is win + playerSelection + 'wins against ' + computerSelection
+    change comment and update scores for win
     */
-    if (playerSelection === computerSelection) {
-        result = 'Draw';
-    } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissor') || (playerSelection === 'scissor' && computerSelection === 'rock')) {
-        result = `Loss`;
+    // else change comment for draw
+    if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissor') || (playerSelection === 'scissor' && computerSelection === 'rock')) {
+        changeComment('loss', playerSelection, computerSelection);
+        updateScores('loss');
     } else if ((playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissor' && computerSelection === 'paper') || (playerSelection === 'rock' && computerSelection === 'scissor')) {
-        result = `Win`;
-    }
-
-    // return result 
-    return result;
-}
-
-// main function for playing the game
-function game() {
-    // declare two variables win and loss and set it initially to 0
-    let win = 0, loss = 0;
-    // declare another variable for 'for' loop
-    let i;
-    // start a for loop from 0 to 4
-    for (i = 0; i < 5; i++) {
-        /* (1) input a weapon from the user and store it in playerSelection
-        (2) call the computerPlay function and store the value in computerSelection 
-        (3) call the playRound function and pass playerSelection and computerSelection as parameters. Store the return value in result.
-        (4) if result == 'Win' 
-                (a) increase win counter by 1
-                (b) print 'You won this round'
-        (5) if result == 'Loss'
-                (a) increase loss counter by 1
-                (b) print 'You lost this round'
-        (6) else print 'This round is a draw'
-        */
-        let playerSelection = prompt('Enter the name of your weapon');
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
-
-        if (result === 'Win') {
-            win++;
-            console.log('You won this round!');
-        } else if (result === 'Draw') {
-            loss++;
-            console.log('You lost this round!');
-        } else {
-            console.log('This round was a draw!');
-        }
-    }
-    // if win > loss print 'Congratulations! You won the game!'
-    // else if loss > win print 'Sorry, You lost the game!'
-    // else print 'You drew with the computer' 
-    
-    if (win > loss) {
-        console.log('Congratulations! You won the game!');
-    } else if (loss > win) {
-        console.log('Sorry, You lost the game!');
+        changeComment('win', playerSelection, computerSelection);
+        updateScores('win');
     } else {
-        console.log('You drew with the computer');
+        changeComment('draw', playerSelection, computerSelection)
     }
 }
+
+// game over after one of the players gets a score of 5
+function gameOver() {
+    if (userScore > computerScore) {
+        comments.textContent = 'Congratulations! You have won the game. Press any button to refresh'
+    } else if (computerScore > userScore) {
+        comments.textContent = 'Sorry! You have lost the game. Press any button to refresh'
+    }
+    
+    window.addEventListener('keydown', () => window.location.reload());
+}
+
+// attaching event listener for each button
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (userScore < 5 && computerScore < 5) {
+            playRound(e)
+        } else {
+            gameOver();
+        }
+    });
+});
+
+
